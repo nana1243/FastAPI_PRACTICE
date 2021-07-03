a939426e9c0ddcafa8983ac35a0d53c4a228e957
nana1243 <hieun1243@gmail.com>
2021-07-03 11:43:50 +0900

    fea) 수정 X

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,13 +1,8 @@
 # 1.FAST API 임포트
-from typing import Optional
 
 from fastapi import FastAPI
-from enum import Enum
-
-from pydantic import BaseModel
 
 import views
 
-app = FastAPI() #2. FastAPI instance 생성성
-
+app = FastAPI()
 app.include_router(views.router)
