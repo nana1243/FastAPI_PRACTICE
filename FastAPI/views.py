02264f06e4fd9cf13a21fe13d50396819bfee7e3
nana1243 <hieun1243@gmail.com>
2021-07-21 23:21:17 +0900

    ƒea) HTTPBase

--- FastAPI/views.py
+++ FastAPI/views.py
@@ -1,11 +1,14 @@
-from typing import Optional, List
+from typing import List, Optional
 
-from fastapi import APIRouter, Query, Path
+from fastapi import APIRouter, Query, Path, Depends, Request
+from fastapi.security import APIKeyHeader
+from fastapi.security.http import HTTPBase
 
 from constants import ModelName
 from models import Item, db, Posts
 
 router = APIRouter()
+
 
 @router.get("/")
 async def root():
@@ -17,7 +20,7 @@ async def root():
 #     return {"item_id" : item_id}
 
 
-#경로 동작은 순차적으로 평가된다.
+# 경로 동작은 순차적으로 평가된다.
 
 @router.get("/users/me")
 async def read_user_me():
@@ -29,10 +32,8 @@ async def read_user(user_id: str):
     return {"user_id": user_id}
 
 
-
-
 @router.get("/models/{model_name}")
-async def get_model(model_name : ModelName):
+async def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
     if model_name.value == "lenet":
@@ -40,10 +41,10 @@ async def get_model(model_name : ModelName):
 
     return {"model_name": model_name, "message": "Have some residuals"}
 
-@router.get("/files/{file_path:path}")
-async def get_files(file_path :str):
-    return {"file_path": file_path}
 
+@router.get("/files/{file_path:path}")
+async def get_files(file_path: str):
+    return {"file_path": file_path}
 
 
 # 쿼리 매개 변수
@@ -64,7 +65,7 @@ async def get_files(file_path :str):
 
 
 @router.post("/items/")
-async def create_item(item : Item):
+async def create_item(item: Item):
     return item
 
 
@@ -87,13 +88,10 @@ async def create_item(item : Item):
 #         results.update({"q":q})
 #     return results
 
-
 @router.get("/items/")
-async def read_items(q:List[str]= Query(["test1","test2"])):
-    query_items = {"q":q}
+async def read_items(q: List[str] = Query(["test1", "test2"])):
+    query_items = {"q": q}
     return query_items
-
-
 
 
 # @router.get("/items/{item_id}")
@@ -107,18 +105,16 @@ async def read_items(q:List[str]= Query(["test1","test2"])):
 #     return results
 
 
-
-#Number validations: greater than or equal¶
+# Number validations: greater than or equal¶
 
 @router.get("/items/{item_id}")
 async def read_items(
-    *, item_id: int = Path(..., title="The ID of the item to get", ge=1,le=1000), q: str,
+        *, item_id: int = Path(..., title="The ID of the item to get", ge=1, le=1000), q: str,
 ):
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
     return results
-
 
 
 @router.get('/posts')
@@ -128,3 +124,47 @@ async def list_posts():
         posts.append(Posts(**post))
     return {'posts': posts}
 
+
+class APIKeyEnterprise(APIKeyHeader):
+    def __init__(self):
+        super().__init__(name="ViodioAK")
+        print()
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        api_key = await super().__call__(request)
+        print('this is apikey!')
+        print(api_key)
+        return api_key
+
+
+def current_enterprise(api_key: str = Depends(APIKeyEnterprise())) -> bool:
+    print(api_key, "test!")
+    return True
+
+
+class APIKeyEnterprise2(HTTPBase):
+    def __init__(self):
+        super().__init__(scheme="ViodioAK")
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        api_key = await super().__call__(request)
+        print("!!!!!")
+        print(api_key.scheme)
+        return api_key
+
+
+def current_enterprise2(api_key: str = Depends(APIKeyEnterprise2())):
+    return True
+
+
+
+@router.get("/items/{item_id}")
+def read_item(item_id: int, test=Depends(current_enterprise), q: Optional[str] = None):
+    print("test is : ", test)
+    return {"item_id": item_id, "q": q}
+
+
+@router.get("/items")
+def read_item(test=Depends(current_enterprise2), q: Optional[str] = None):
+    print("test is : ", test)
+    return {"test": ["teset1", "test2"]}
